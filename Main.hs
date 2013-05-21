87ac41807396e98dea3b14c3340cc102e36178bb
Vitaly Turinsky <vitaly.turinsky@gmail.com>
2013-05-21 19:00:33 +0300

    Liberties, captures

--- Main.hs
+++ Main.hs
@@ -4,9 +4,10 @@ import Board
 import Rules
 import System.Environment
 import Data.Char ( toLower )
-import Data.Lens.Lazy ( (^=), (^.) )
+import Data.Lens.Lazy ( (^=), (^.), (^+=) )
 import Data.Lens.Template ( makeLenses )
-import Control.Monad ( unless )
+import Control.Monad ()
+import Control.Monad.State
 
 data GameOptions = GameOptions
                     { _boardSize :: Int
@@ -16,7 +17,8 @@ data GameOptions = GameOptions
 
 data GameState = GameState
                   { _curBoard :: Board.Board
-                  , _score :: Float  -- Black : positive, White : negative
+                  , _scoreTerritory :: Float  -- Black : positive, White : negative
+                  , _scoreStones :: Int
                   , _whoseTurn :: Cell
                   , _passes :: Int
                   , _prevBoard :: Board.Board }
@@ -37,7 +39,8 @@ defaultOptions = GameOptions { _boardSize = 9,
 
 startingState :: GameOptions -> GameState
 startingState options = GameState { _curBoard = empty_board,
-                                    _score = options^.komi,
+                                    _scoreTerritory = options^.komi,
+                                    _scoreStones = 0,
                                     _whoseTurn = Black,
                                     _passes = 0,
                                     _prevBoard = empty_board }
@@ -53,8 +56,8 @@ getBoardSize = do putStrLn "Choose board size: s - Small (9), m - Medium (13), l
                        | otherwise                      -> do putStrLn "Wrong! Try again"
                                                               getBoardSize
 
-menuLoop :: [String] -> GameOptions -> IO ()
-menuLoop args options = do
+menuLoop :: GameOptions -> [String] -> IO ()
+menuLoop options args = do
     putStrLn $ "Options: " ++ show options
     putStrLn "1. Single player"
     putStrLn "2. Multiplayer"
@@ -65,45 +68,61 @@ menuLoop args options = do
     choice <- getLine
     case choice of
          "2"       -> do startMultiplayer options
-                         menuLoop args options
+                         menuLoop options args
          "5"       -> do newBoardSize <- getBoardSize
                          let newOptions = boardSize ^= newBoardSize $ options
-                         menuLoop args newOptions
+                         menuLoop newOptions args
          "6"       -> return ()
-         _         -> menuLoop args options
+         _         -> menuLoop options args
 
 startMultiplayer :: GameOptions -> IO ()
 startMultiplayer options =
     multiplayerWorker options $ startingState options
 
 multiplayerWorker :: GameOptions -> GameState -> IO ()
-multiplayerWorker options state = do
-    newState <- makeTurn state
-    let newState1 = whoseTurn ^= opposite (state^.whoseTurn) $ newState
+multiplayerWorker options st = do
+    newState <- makeTurn st
+    let newState1 = whoseTurn ^= opposite (st^.whoseTurn) $ newState
     unless (newState1^.passes > 1) $ multiplayerWorker options newState1
 
 makeTurn :: GameState -> IO GameState
-makeTurn state = do
-    putStrLn $ "Score: (-) White <--- " ++ show (state^.score) ++ " ---> Black (+)"
-    putStr   $ Board.showAnnotated $ state^.curBoard
+makeTurn st = do
+    putStrLn $ "Score: (-) White <--- " ++ show ((st^.scoreTerritory) + fromIntegral (st^.scoreStones)) ++ " ---> Black (+)"
+    putStr   $ Board.showAnnotated $ st^.curBoard
     printGroups Black
     printGroups White
     readTurn
-    where printGroups col = putStr $ show col ++ " groups:\n" ++ showGroups (groups (state^.curBoard) col)
+    where printGroups col = putStr $ show col ++ " groups:\n" ++ showGroupsWithLib (st^.curBoard) (groups (st^.curBoard) col)
           readTurn = do
-            let color = state^.whoseTurn
+            let color = st^.whoseTurn
             putStrLn $ show color ++ "'s turn:"
             move <- getLine
             let parsed = case fromGoCoord move of
                                 Nothing       -> Left "Failed to parse (hint: use coordinates, ex. D4)"
                                 Just (-1, -1) -> Left "pass"
                                 Just (i, j)   -> Right (i, j)
-            let board = state^.curBoard
-            let action = parsed >>= checkSize board >>= checkOccupied board >>= checkSuicide board >>= checkKo board
+            let board = st^.curBoard
+            let action = parsed >>= checkSize board >>= checkOccupied board
+                                >>= checkSuicide board color >>= checkKo (st^.prevBoard) board color
             case action of
-                 Left err | err == "pass" -> return $ passes ^= (state^.passes + 1) $ state
+                 Left err | err == "pass" -> return $ passes ^= (st^.passes + 1) $ st
                           | otherwise     -> putStrLn ("Wrong move: " ++ err) >> readTurn
-                 Right (i, j)   -> return $ (prevBoard ^= board) . (curBoard ^= Board.replace board color i j) $ state
+                 Right (i, j)             -> return $ execState (execMove (i, j)) st
+
+execMove :: BoardCoord -> State GameState ()
+execMove coord = do
+    color <- getColor
+    saveBoard
+    addStone coord color
+    capt <- captured color
+    removeCaptured capt
+    updateScore (if color == Black then length capt else - length capt)
+    where getColor = state $ \s -> (s^.whoseTurn, s)
+          saveBoard = state $ \s -> ((), prevBoard ^= (s^.curBoard) $ s)
+          addStone (i, j) col = state $ \s -> ((), curBoard ^= replace (s^.curBoard) col i j $ s)
+          captured col = state $ \s -> (getCaptured (s^.curBoard) $ opposite col, s)
+          removeCaptured capt = state $ \s -> ((), curBoard ^= replaceAll (s^.curBoard) Empty capt $ s)
+          updateScore val = state $ \s -> ((), scoreStones ^+= val $ s)
 
 checkSize :: Board -> BoardCoord -> Either String BoardCoord
 checkSize board (i, j) | i >= size || j >= size = Left "Coordinates are too big"
@@ -111,5 +130,5 @@ checkSize board (i, j) | i >= size || j >= size = Left "Coordinates are too big"
     where size = getSize board
 
 main :: IO ()
-main = getArgs >>= \a -> menuLoop a defaultOptions
+main = getArgs >>= menuLoop defaultOptions
 
--- Rules.hs
+++ Rules.hs
@@ -1,7 +1,10 @@
 module Rules
 ( GroupMap
 , groups
+, liberties
 , showGroups
+, showGroupsWithLib
+, getCaptured
 , checkSuicide
 , checkKo
 ) where
@@ -10,13 +13,14 @@ module Rules
 import Board
 import Data.Maybe
 import Data.Function
-import Data.List
+import qualified Data.List as L
+import qualified Data.Set as S
 import qualified Data.Map as M
 
-type BoardMap = M.Map BoardCoord Board.Cell
 type GroupMap = M.Map BoardCoord Int
 
 {-
+type BoardMap = M.Map BoardCoord Board.Cell
 -- convert board from list of lists to map from coords to color
 -- simplifies analisys
 toMap :: Board.Board -> BoardMap
@@ -26,8 +30,11 @@ toMap board = toMap' M.empty board 0 0
           toMap' m ((cell:line):lines) i j = toMap' (M.insert (i, j) cell m) (line:lines) i (j+1)
 -}
 
+neighbors :: Int -> Int -> [BoardCoord]
+neighbors i j = [(i, j-1), (i-1, j), (i, j+1), (i+1, j)]
+
 groups :: Board -> Cell -> [[BoardCoord]]
-groups board color = map (map fst) $ groupBy ((==) `on` snd) $ sortBy (compare `on` snd) (M.toList groupMap)
+groups board color = map (map fst) $ L.groupBy ((==) `on` snd) $ L.sortBy (compare `on` snd) (M.toList groupMap)
     where groupMap = groups' board (0, 0) M.empty 0
 
           size = getSize board
@@ -41,19 +48,45 @@ groups board color = map (map fst) $ groupBy ((==) `on` snd) $ sortBy (compare `
           propagate (i, j) gr_id grmap
                 | i < 0 || j < 0 || i >= size || j >= size = grmap
                 | M.member (i, j) grmap                    = grmap
-                | ((board !! i) !! j) /= color              = grmap
-                | otherwise = foldl (\acc x -> propagate x gr_id acc) inserted neighbors
+                | ((board !! i) !! j) /= color             = grmap
+                | otherwise = foldl (\acc x -> propagate x gr_id acc) inserted (neighbors i j)
             where inserted = M.insert (i, j) gr_id grmap
-                  neighbors = [(i, j-1), (i-1, j), (i, j+1), (i+1, j)]
 
 showGroups :: [[BoardCoord]] -> String
-showGroups [] = "\n"
-showGroups (gr:grs) = "( " ++ showGroup gr ++ " )\n" ++ showGroups grs
-    where showGroup = foldl (\acc x -> acc ++ " " ++ fromMaybe "Error" (toGoCoord x)) []
+showGroups = foldl (\acc x -> acc ++ "( " ++ showGroup x ++ " )\n") []
 
-checkSuicide :: Board -> BoardCoord -> Either String BoardCoord
-checkSuicide board = Right
+showGroupsWithLib :: Board -> [[BoardCoord]] -> String
+showGroupsWithLib board = foldl (\acc x -> acc ++ "( " ++ showGroup x ++ " ) : L = " ++ show (liberties board x) ++ "\n") []
 
-checkKo :: Board -> BoardCoord -> Either String BoardCoord
-checkKo board = Right
+showGroup :: [BoardCoord] -> String
+showGroup = foldl (\acc x -> acc ++ " " ++ fromMaybe "Error" (toGoCoord x)) []
+
+liberties :: Board -> [BoardCoord] -> Int
+liberties board group = S.size $ liberties' group
+    where liberties' []            = S.empty
+          liberties' ((i, j):rest) = S.union (liberties' rest) $ S.fromList (filter isEmpty (neighbors i j))
+          size = getSize board
+          isEmpty (i, j)
+                | i < 0 || j < 0 || i >= size || j >= size = False
+                | otherwise = ((board !! i) !! j) == Empty
+
+getCaptured :: Board -> Cell -> [BoardCoord]
+getCaptured board color = fromMaybe [] $ L.find (\gr -> liberties board gr == 0) (groups board color)
+
+-- assumes that BoardCoord is Empty
+-- For move to be suicidal, it needs to create a group with 0 liberties, but
+-- liberties should be counted after all captures have happened
+checkSuicide :: Board -> Cell -> BoardCoord -> Either String BoardCoord
+checkSuicide board color coord
+        | liberties newBoard myGroup == 0 = Left "No fucking way... (It would be a suicide)"
+        | otherwise = Right coord
+    where simBoard = replaceAll board color [coord]
+          newBoard = replaceAll simBoard Empty $ getCaptured simBoard (opposite color)
+          myGroup = fromMaybe [coord] $ L.find (\sublist -> coord `elem` sublist) (groups newBoard color)
+
+
+-- Ko rules forbids to revert board state to the previous one
+-- it prevents endless loops
+checkKo :: Board -> Board -> Cell -> BoardCoord -> Either String BoardCoord
+checkKo prevBoard curBoard color = Right
 
